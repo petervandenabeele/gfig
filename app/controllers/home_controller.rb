6f4a2eac53efded38daee6fc4b00aed31cf82c03
Peter Vandenabeele <peter@vandenabeele.com>
2014-03-22 12:46:13 +0100

    Target is shown in the GUI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
   respond_to :html
 
   def index
+    @target = Target.new.read_target
   end
 
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,9 +4,16 @@ describe HomeController do
 
   context 'GET index' do
 
+    render_views
+
     it 'has a 200 status code' do
       get('index')
       expect(response.code).to eq '200'
+    end
+
+    it 'has the word Green, Orange, Red' do
+      get('index')
+      expect(response.body).to match(/(Green)|(Orange)|(Red)/)
     end
   end
 end
