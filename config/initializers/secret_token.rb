11bb855c111a49bd3b67a025043d7cef33e321cc
Peter Vandenabeele <peter@vandenabeele.com>
2014-03-22 12:01:42 +0100

    Initial rails 4.0.4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gfig::Application.config.secret_key_base = 'c924f61c25ebd2f9ce502782d0fbf78c4b541b684280fcda5020ac5f97e34a3d781cbae1b088350c69cfa74d6ff5802179e6d023eba3e46f6214c10ae260d27f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gfig::Application.config.session_store :cookie_store, key: '_gfig_session'
